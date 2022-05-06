f373e70ab89a91354255133ecbe0c212236547f4
David Tanner <david.tanner@lifeomic.com>
2022-05-06 14:34:56 -0600

    Allow mutable installs

--- packages/webpack/test/integration/setup.sh
+++ packages/webpack/test/integration/setup.sh
@@ -5,21 +5,20 @@ set -e
 
 DIRECTORY="$(cd $(dirname ${BASH_SOURCE}); pwd)"
 
-set -e
+export YARN_ENABLE_IMMUTABLE_INSTALLS=false
 
 SILENT_LOG="${DIRECTORY}/silent_log_$$.txt"
 trap_add "/bin/rm -f $SILENT_LOG" EXIT
 
 function report_and_exit {
   cat "${SILENT_LOG}";
-  echo "\033[91mError running command.\033[39m"
+  echo "Error running command."
   exit 1;
 }
 
 function silent {
   $* 2>>"${SILENT_LOG}" >> "${SILENT_LOG}" || report_and_exit;
 }
-
 
 function installNpmProject () {
   PROJECT_DIR="${INTEGRATION_TEST_TMP_DIR}/$1"
